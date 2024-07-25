c3ca317a9a169085e83a8992703623f50aa77e0a
kerkeruil <kerkeruil@gmail.com>
2024-07-25 15:35:11 +0200

    all anims working, time to start thinking of how it should fight

--- src/main/java/kerkeruil/dope_dunes/entity/custom/TestBossEntity.java
+++ src/main/java/kerkeruil/dope_dunes/entity/custom/TestBossEntity.java
@@ -1,12 +1,12 @@
 package kerkeruil.dope_dunes.entity.custom;
 
+import com.mojang.datafixers.util.Pair;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.brain.Brain;
-import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
@@ -26,9 +26,7 @@ import net.tslat.smartbrainlib.api.core.behaviour.FirstApplicableBehaviour;
 import net.tslat.smartbrainlib.api.core.behaviour.OneRandomBehaviour;
 import net.tslat.smartbrainlib.api.core.behaviour.custom.attack.AnimatableMeleeAttack;
 import net.tslat.smartbrainlib.api.core.behaviour.custom.look.LookAtTarget;
-import net.tslat.smartbrainlib.api.core.behaviour.custom.misc.Idle;
 import net.tslat.smartbrainlib.api.core.behaviour.custom.move.MoveToWalkTarget;
-import net.tslat.smartbrainlib.api.core.behaviour.custom.move.StrafeTarget;
 import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetRandomWalkTarget;
 import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetWalkTargetToAttackTarget;
 import net.tslat.smartbrainlib.api.core.behaviour.custom.target.InvalidateAttackTarget;
@@ -38,6 +36,7 @@ import net.tslat.smartbrainlib.api.core.behaviour.custom.target.TargetOrRetaliat
 import net.tslat.smartbrainlib.api.core.sensor.ExtendedSensor;
 import net.tslat.smartbrainlib.api.core.sensor.vanilla.NearbyLivingEntitySensor;
 import net.tslat.smartbrainlib.api.core.sensor.vanilla.NearbyPlayersSensor;
+import net.tslat.smartbrainlib.util.BrainUtils;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
@@ -48,8 +47,8 @@ import software.bernie.geckolib.util.GeckoLibUtil;
 import java.util.List;
 
 public class TestBossEntity extends HostileEntity implements GeoEntity, SmartBrainOwner<TestBossEntity> {
-
     private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache(this);
+    private static boolean MELEE_ATTACK = false;
 
     public TestBossEntity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
@@ -99,8 +98,15 @@ public class TestBossEntity extends HostileEntity implements GeoEntity, SmartBra
         return BrainActivityGroup.fightTasks(
                 new InvalidateAttackTarget<>(), // Cancel fighting if the target is no longer valid
                 new SetWalkTargetToAttackTarget<>(),      // Set the walk target to the attack target
-                new AnimatableMeleeAttack<>(0)); // Melee attack the target if close enough
+//                new AnimatableMeleeAttack<>(0)); // Melee attack the target if close enough
+                new OneRandomBehaviour<>(
+                    Pair.of(new AnimatableMeleeAttack<>(10).attackInterval(entity -> 10)
+                            .whenStarting(entity -> MELEE_ATTACK = true) //TODO
+                            .whenStopping(entity -> MELEE_ATTACK = false), 5)
+                ).startCondition(entity -> !BrainUtils.isOnSpecialCooldown(this))
+        );
     }
+
 
     @Override
     protected void initGoals() {
@@ -129,7 +135,7 @@ public class TestBossEntity extends HostileEntity implements GeoEntity, SmartBra
     public static DefaultAttributeContainer.Builder createTestBossAttributes() {
         return HostileEntity.createHostileAttributes()
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 10.0)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.1)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.2) // If too low, animation won't play
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 3.0)
                 .add(EntityAttributes.GENERIC_ARMOR, 2.0)
                 .add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK,5);
@@ -169,18 +175,27 @@ public class TestBossEntity extends HostileEntity implements GeoEntity, SmartBra
         return EntityGroup.UNDEAD;
     }
 
-//    GECKOLIB STUFF
+    // GECKOLIB STUFF
+    public static RawAnimation WALKING = RawAnimation.begin().then("animation.test_boss.walk", Animation.LoopType.LOOP);
+    public static RawAnimation STANDING = RawAnimation.begin().then("animation.test_boss.stand", Animation.LoopType.LOOP);
+    public static RawAnimation ATTACKING = RawAnimation.begin().then("animation.test_boss.attack", Animation.LoopType.LOOP);
+
     private PlayState predicate(AnimationState<TestBossEntity> testBossEntityAnimationState) {
-        if(testBossEntityAnimationState.isMoving()) {
-            testBossEntityAnimationState.getController().setAnimation(RawAnimation.begin().then("animation.test_boss.walk", Animation.LoopType.LOOP));
+        if(MELEE_ATTACK){
+            testBossEntityAnimationState.getController().setAnimation(ATTACKING);
             return PlayState.CONTINUE;
         }
-        testBossEntityAnimationState.getController().setAnimation(RawAnimation.begin().then("animation.test_boss.stand", Animation.LoopType.LOOP));
+        else if(testBossEntityAnimationState.isMoving()) {
+            testBossEntityAnimationState.getController().setAnimation(WALKING);
+            return PlayState.CONTINUE;
+        }
+        testBossEntityAnimationState.getController().setAnimation(STANDING);
         return PlayState.CONTINUE;
     }
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+        // No 2 controllers can control the same bone at the same time
         controllers.add(new AnimationController<>(this, "controller", 5, this::predicate));
     }
 
